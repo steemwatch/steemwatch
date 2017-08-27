ab295fb3fa583a7883903b0c8a0350f7f91d5cd9
Ondřej Kupka <ondra.cap@gmail.com>
2017-08-27 22:31:20 +0200

    wip [ci skip]

--- cmd/event_emitter/event_emitter.go
+++ cmd/event_emitter/event_emitter.go
@@ -35,43 +35,54 @@ func NewEventEmitter(
 }
 
 func (ee *EventEmitter) Start() error {
-	sc, err := stan.Connect(
-		ee.config.STANClusterID,
-		ee.config.STANClientID,
-		stan.NatsURL(ee.config.STANURL),
-		stan.ConnectWait(ee.config.STANConnectWait),
-		stan.PubAckWait(ee.config.STANPubAckWait),
-	)
-	if err != nil {
-		return ee.nuked(
-			err, "failed to connect to STAN",
+	ee.t.Go(func() error {
+		ee.logger.Info("started")
+		defer ee.logger.Info("terminated")
+
+		sc, err := stan.Connect(
+			ee.config.STANClusterID,
+			ee.config.STANClientID,
+			stan.NatsURL(ee.config.STANURL),
+			stan.ConnectWait(ee.config.STANConnectWait),
+			stan.PubAckWait(ee.config.STANPubAckWait),
+		)
+		if err != nil {
+			return ee.nuked(
+				err, "failed to connect to STAN",
+				zap.String("url", ee.config.STANURL),
+				zap.String("cluster_id", ee.config.STANClusterID),
+				zap.String("client_id", ee.config.STANClientID),
+				zap.Error(err),
+			)
+		}
+
+		ee.logger.Info(
+			"connected to STAN",
 			zap.String("url", ee.config.STANURL),
 			zap.String("cluster_id", ee.config.STANClusterID),
 			zap.String("client_id", ee.config.STANClientID),
-			zap.Error(err),
 		)
-	}
 
-	ee.logger.Info(
-		"connected to STAN",
-		zap.String("url", ee.config.STANURL),
-		zap.String("cluster_id", ee.config.STANClusterID),
-		zap.String("client_id", ee.config.STANClientID),
-	)
+		if _, err := sc.QueueSubscribe(
+			ee.config.STANInputSubject,
+			ee.config.ServiceName,
+			ee.handleMessage,
+			stan.DurableName(ee.config.ServiceName),
+		); err != nil {
+			return ee.nuked(
+				err, "failed to subscribe to the input subject",
+				zap.String("subject", ee.config.STANInputSubject),
+			)
+		}
 
-	if _, err := sc.QueueSubscribe(
-		ee.config.STANInputSubject,
-		ee.config.ServiceName,
-		ee.handleMessage,
-		stan.DurableName(ee.config.ServiceName),
-	); err != nil {
-		return ee.nuked(
-			err, "failed to subscribe to the input subject",
-			zap.String("subject", ee.config.STANInputSubject),
-		)
-	}
+		// Wait for the termination signal.
+		<-ee.t.Dying()
 
-	ee.sc = sc
+		if err := sc.Close(); err != nil {
+			ee.nuked(err, "failed to close STAN connection")
+		}
+		return nil
+	})
 	return nil
 }
 
@@ -81,14 +92,11 @@ func (ee *EventEmitter) Stop(ctx context.Context) error {
 }
 
 func (ee *EventEmitter) Wait() error {
-	err := ee.t.Wait()
-	ee.logger.Info("terminated")
-	return err
+	return ee.t.Wait()
 }
 
-func (ee *EventEmitter) loop() error {
-	for {
-	}
+func (ee *EventEmitter) handleMessage(msg *stan.Msg) {
+
 }
 
 func (ee *EventEmitter) nuked(
